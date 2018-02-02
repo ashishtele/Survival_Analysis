d6c9e92da881f47ad2d263c559d240dd0faa49d9
ashishtele <sale.ashish.tele@gmail.com>
2018-02-02 14:34:48 -0500

    Survival analysis - Kaplan meier method

---
+++ Survival Analysis.R
@@ -0,0 +1,29 @@
+##Importing the data
+
+
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(survival)
+cancer <- read_excel("E:\\Study\\R Projects\\Survival/Cancer_data.xlsx")
+
+tbl_df(cancer)
+glimpse(cancer)
+
+##Data manipilation
+
+sum(is.na(cancer))
+
+red_data <- cancer[,c("Survival_days","Status","Prior_therapy")]
+tbl_df(red_data)
+
+##K-M model
+
+attach(red_data)
+KM <- survfit(Surv(Survival_days,Status)~Prior_therapy)
+summary(KM)
+
+##Plot
+plot(KM)
+
+
